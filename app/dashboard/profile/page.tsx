4613f2334bf92280f98aeb983acb2ba389884be9
rijan shiwakoti <rijankrishna14@gmail.com>
2026-06-23 14:13:10 +0545

    Added ProfilePage

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,8 @@
+import { getUserData } from "@/lib/cookies";
+import { ProfileClient, type UserInfo } from "../_components/profile-client";
+
+export default async function Page() {
+  const cookieUser = (await getUserData()) as UserInfo;
+
+  return <ProfileClient initialUser={cookieUser} />;
+}
